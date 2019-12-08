cbcf68efa2aef08a3b21e6a18b3fce1303872800
Luís Paim <luis.paimadv@gmail.com>
2019-12-08 10:33:12 -0300

    teste validator 8

--- docassemble/validatorscollectionbr/validators_docassemble_wrapper.py
+++ docassemble/validatorscollectionbr/validators_docassemble_wrapper.py
@@ -28,3 +28,17 @@ def validate_cnpj(value):
         return False
     else:
         return True
+
+def validate_cnj(value):
+    # ao executar a entrevista o docassemble executa as validações
+    # antes do usuario preencher o campo, por isso só deve fazer a
+    # validação se o campo estiver preenchido
+    if not value:
+        return True
+    
+    msg = validator_cnj(value)
+    if msg != True:
+        validation_error(msg)
+        return False
+    else:
+        return True
