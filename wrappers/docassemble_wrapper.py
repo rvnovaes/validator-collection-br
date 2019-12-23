f431e6500e67f24010570ccb4f9f99390327fd3a
Luispaim <luis.paimadv@gmail.com>
2019-12-23 11:45:55 -0300

    criacao do wrapper cnpj

--- wrappers/docassemble_wrapper.py
+++ wrappers/docassemble_wrapper.py
@@ -4,50 +4,32 @@ from docassemble.base.util import validation_error
 
 def validate_cpf(value):
     # ao executar a entrevista o docassemble executa as validações
-    # antes do usuario preencher o campo, por isso só deve fazer a
-    # validação se o campo estiver preenchido
-    # if not value:
-    #     return True
+    # antes do usuario preencher o campo,
+    # por isso só deve fazer a validação se o campo estiver preenchido
+    if not value:
+        return True
+
+    # passando o valor do docassemble para a função de validação
     try:
         value = validators_br.cpf(value)
     except Exception as e:
         msg = validation_error(e.value)
         validation_error(msg)
 
-    return value
+    return True
 
-    # if msg != True:
-    #     validation_error(msg)
-    #     return False
-    # else:
-    #     return True
+def validate_cnpj(value):
+    # ao executar a entrevista o docassemble executa as validações
+    # antes do usuario preencher o campo,
+    # por isso só deve fazer a validação se o campo estiver preenchido
+    if not value:
+        return True
 
+    # passando o valor do docassemble para a função de validação
+    try:
+        value = validators_br.cnpj(value)
+    except Exception as e:
+        msg = validation_error(e.value)
+        validation_error(msg)
 
-# def validate_cnpj(value):
-#     # ao executar a entrevista o docassemble executa as validações
-#     # antes do usuario preencher o campo, por isso só deve fazer a
-#     # validação se o campo estiver preenchido
-#     if not value:
-#         return True
-#
-#     msg = validator_cnpj(value)
-#     if msg != True:
-#         validation_error(msg)
-#         return False
-#     else:
-#         return True
-#
-#
-# def validate_cnj(value):
-#     # ao executar a entrevista o docassemble executa as validações
-#     # antes do usuario preencher o campo, por isso só deve fazer a
-#     # validação se o campo estiver preenchido
-#     if not value:
-#         return True
-#
-#     msg = validator_cnj(value)
-#     if msg != True:
-#         validation_error(msg)
-#         return False
-#     else:
-#         return True
+    return True
